1e78ffc73d36daaf7f2f734724f80ce4426daf82
Rangaswamicse <k.rangaswamy6301@gmail.com>
2024-11-13 22:51:22 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,21 @@
+public class main{
+ int i;
+ int j;  
+  public main(){
+    this.i=10;
+    this.j=20;
+
+  }
+    public static void main(String[] args) {
+      main s1= new main();
+      System.out.println(s1.add());
+      public int add() {
+      return i+j;
+      }
+
+    }
+}
+
+    
+  
+    
